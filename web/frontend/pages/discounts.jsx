384bb58ee263f2799bc519e208391b12e9f89451
Vidhee <vidhee.luckimedia@gmail.com>
2023-12-28 19:33:53 +0530

    discount module & uninstall webhook

--- web/frontend/pages/discounts.jsx
+++ web/frontend/pages/discounts.jsx
@@ -1,54 +1,137 @@
-import { Card, Page, Layout, TextContainer, Text } from "@shopify/polaris";
-import { TitleBar } from "@shopify/app-bridge-react";
-import { useTranslation } from "react-i18next";
+import {
+  IndexTable,
+  LegacyCard,
+  Text,
+  Badge,
+  useBreakpoints,
+  Checkbox,
+} from "@shopify/polaris";
+import React, { useCallback, useState } from "react";
 
 export default function PageName() {
-  const { t } = useTranslation();
+  const orders = [
+    {
+      id: "1020",
+      order: "#1020",
+      date: "Jul 20 at 4:34pm",
+      customer: "Jaydon Stanton",
+      total: "$969.44",
+      paymentStatus: <Badge progress="complete">Paid</Badge>,
+      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+    },
+    {
+      id: "1019",
+      order: "#1019",
+      date: "Jul 20 at 3:46pm",
+      customer: "Ruben Westerfelt",
+      total: "$701.19",
+      paymentStatus: <Badge progress="partiallyComplete">Partially paid</Badge>,
+      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+    },
+    {
+      id: "1018",
+      order: "#1018",
+      date: "Jul 20 at 3.44pm",
+      customer: "Leo Carder",
+      total: "$798.24",
+      paymentStatus: <Badge progress="complete">Paid</Badge>,
+      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+    },
+  ];
+  const resourceName = {
+    singular: "order",
+    plural: "orders",
+  };
+
+  const rowMarkup = orders.map(
+    (
+      { id, order, date, customer, total, paymentStatus, fulfillmentStatus },
+      index
+    ) => (
+      <IndexTable.Row id={id} key={id} position={index}>
+        <IndexTable.Cell>
+          <Text variant="bodyMd" fontWeight="bold" as="span">
+            {order}
+          </Text>
+        </IndexTable.Cell>
+        <IndexTable.Cell>{date}</IndexTable.Cell>
+        <IndexTable.Cell>{customer}</IndexTable.Cell>
+        <IndexTable.Cell>
+          <Text as="span" alignment="end" numeric>
+            {total}
+          </Text>
+        </IndexTable.Cell>
+        <IndexTable.Cell>{paymentStatus}</IndexTable.Cell>
+        <IndexTable.Cell>{fulfillmentStatus}</IndexTable.Cell>
+      </IndexTable.Row>
+    )
+  );
+
+  const [checked, setChecked] = useState(false);
+  const handleChange = useCallback((newChecked) => setChecked(newChecked), []);
+
   return (
-    <Page>
-      <TitleBar
-        title="Discount Management"
-        primaryAction={{
-          content: t("PageName.primaryAction"),
-          onAction: () => console.log("Primary heyy"),
-        }}
-        secondaryActions={[
-          {
-            content: t("PageName.secondaryAction"),
-            onAction: () => console.log("Secondary action"),
-          },
-        ]}
-      />
-      <Layout>
-        <Layout.Section>
-          <Card sectioned>
-            <Text variant="headingMd" as="h2">
-              {t("PageName.heading")}
-            </Text>
-            <TextContainer>
-              <p>{t("PageName.body")}</p>
-            </TextContainer>
-          </Card>
-          <Card sectioned>
-            <Text variant="headingMd" as="h2">
-              {t("PageName.heading")}
-            </Text>
-            <TextContainer>
-              <p>{t("PageName.body")}</p>
-            </TextContainer>
-          </Card>
-        </Layout.Section>
-        <Layout.Section secondary>
-          <Card sectioned>
-            <Text variant="headingMd" as="h2">
-              {t("PageName.heading")}
-            </Text>
-            <TextContainer>
-              <p>{t("PageName.body")}</p>
-            </TextContainer>
-          </Card>
-        </Layout.Section>
-      </Layout>
-    </Page>
+    <>
+      {/* <div className="discount_fullscreenbar">
+        <FullscreenBar>
+          <div
+            style={{
+              display: "flex",
+              flexGrow: 1,
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingLeft: "1rem",
+              paddingRight: "1rem",
+              background: isSaveButtonDisabled ? "#fff" : "#5488c7",
+              transition: "background 0.5s ease-out 0s",
+            }}
+          >
+            <div
+              style={{
+                flexGrow: 1,
+              }}
+            >
+              <p
+                className="fullscreenbar_headertitle"
+                style={{
+                  color: isSaveButtonDisabled ? "#000" : "#fff",
+                }}
+              >
+                {isSaveButtonDisabled
+                  ? "Discount Management"
+                  : "Unsaved Changes"}
+              </p>
+            </div>
+            <ButtonGroup>
+              <Button
+                variant="primary"
+                disabled={isSaveButtonDisabled}
+                onClick={() => console.log("Primary heyy")}
+              >
+                Save
+              </Button>
+            </ButtonGroup>
+          </div>
+        </FullscreenBar>
+      </div> */}
+      <LegacyCard>
+        <IndexTable
+          condensed={useBreakpoints().smDown}
+          resourceName={resourceName}
+          itemCount={orders.length}
+          headings={[
+            { title: "Order" },
+            { title: "Date" },
+            { title: "Customer" },
+            { title: "Total", alignment: "end" },
+            { title: "Payment status" },
+            { title: "Fulfillment status" },
+          ]}
+          selectable={false}
+        >
+          {rowMarkup}
+        </IndexTable>
+      </LegacyCard>
+    </>
   );
 }
