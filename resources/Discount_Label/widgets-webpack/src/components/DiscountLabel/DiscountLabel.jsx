a3122497fd3adec9acf08aaf1e9109bea2013138
Vidhee <vidhee.luckimedia@gmail.com>
2024-03-28 14:29:05 +0000

    Discount label UI updated, working on Animation

--- resources/Discount_Label/widgets-webpack/src/components/DiscountLabel/DiscountLabel.jsx
+++ resources/Discount_Label/widgets-webpack/src/components/DiscountLabel/DiscountLabel.jsx
@@ -132,7 +132,28 @@ const DiscountLabel = (props) => {
                       .fontSize,
                 }}
               >
-                {props.json_style_data.discount_label_settings.label_style.text}
+                {props.json_style_data.discount_label_settings.label_style.text}{" "}
+                <svg
+                  width={
+                    props.json_style_data.discount_label_settings.icon_style
+                      .size
+                  }
+                  height={
+                    props.json_style_data.discount_label_settings.icon_style
+                      .size
+                  }
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                >
+                  <path
+                    d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"
+                    fill={
+                      props.json_style_data.discount_label_settings.icon_style
+                        .color
+                    }
+                  />
+                </svg>
               </a>
             </div>
           </div>
--- web/frontend/components/Preview/DiscountLabel.jsx
+++ web/frontend/components/Preview/DiscountLabel.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import placeholder from "../../assets/placeholder-image.jpg";
 import style from "../../css/DisccountLabel.module.css";
 import SolidDoubleTags from "../Icons/SolidDoubleTags";
@@ -8,6 +9,19 @@ import ThinPercentageTag from "../Icons/ThinPercentageTag";
 import ThinTag from "../Icons/ThinTag";
 
 export default function DisccountLabel(props) {
+  const [isVibrating, setIsVibrating] = useState(false);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsVibrating(true);
+      setTimeout(() => setIsVibrating(false), 2000); // Duration of the vibrate animation (0.2s)
+
+      // Clear the interval after 5 seconds (5000 milliseconds)
+      setTimeout(() => clearInterval(interval), 5000);
+    }, 5000); // Trigger every 5 seconds (5000 milliseconds)
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(interval);
+  });
   return (
     <div className={style["onclick-popup"]}>
       <div className={style.PreviewTopbar}>
@@ -117,6 +131,40 @@ export default function DisccountLabel(props) {
                       props.json_style_data.discount_label_settings.label_style
                         .text
                     }
+                  </a>
+                  <a
+                    className={`${isVibrating ? style.shake : ""}`}
+                    href="#"
+                    style={{
+                      color:
+                        props.json_style_data.discount_label_settings
+                          .label_style.color,
+                      fontSize:
+                        props.json_style_data.discount_label_settings
+                          .label_style.fontSize,
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      enableBackground="new 0 0 2000 2000"
+                      viewBox="0 0 2000 2000"
+                      id="right-arrow"
+                      width={
+                        props.json_style_data.discount_label_settings
+                          .label_style.fontSize
+                      }
+                      height={
+                        props.json_style_data.discount_label_settings
+                          .label_style.fontSize
+                      }
+                      fill={
+                        props.json_style_data.discount_label_settings
+                          .label_style.color
+                      }
+                    >
+                      <path d="M1743.9,1064H256c-35.3,0-64-28.7-64-64c0-35.3,28.7-64,64-64h1487.9c35.3,0,64,28.7,64,64C1807.9,1035.3,1779.3,1064,1743.9,1064z"></path>
+                      <path d="M1072.4,1735.6c-16.4,0-32.8-6.2-45.3-18.7c-25-25-25-65.5,0-90.5l626.4-626.4l-626.4-626.4c-25-25-25-65.5,0-90.5c25-25,65.5-25,90.5,0l671.6,671.6c12,12,18.7,28.3,18.7,45.3c0,17-6.7,33.3-18.7,45.3l-671.6,671.6C1105.1,1729.4,1088.7,1735.6,1072.4,1735.6z"></path>
+                    </svg>
                   </a>
                 </div>
               </div>
